c5be583aa6a72c45acf56cffa631c67161e90cab
Michael Johansen <michael.johansen@nav.no>
2019-03-01 10:39:56 +0100

    Fixed docker-images

--- fpsak-for-vtp/push-local.sh
+++ fpsak-for-vtp/push-local.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 export RELEASE_VERSION="v$(date +%Y%m%d%H%M)"
-
+export RELEASE_VERSION=latest
 docker build . -t repo.adeo.no:5443/fpsak-for-vtp:${RELEASE_VERSION} -f ./Dockerfile --no-cache
-docker push repo.adeo.no:5443/fpsak-for-vtp:${RELEASE_VERSION}
-
 docker build . -t repo.adeo.no:5443/fpmock2:${RELEASE_VERSION} -f ./DockerfileMock2 --no-cache
-docker push repo.adeo.no:5443/fpmock2:${RELEASE_VERSION}
